38539dd225dc0a534f7a0d09d8adf376827b8bf6
Cătălin Grigoriev <catalingrigoriev50@gmail.com>
2026-02-02 15:29:41 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using wraithspire.engine;
+
+namespace Wraithspire
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            using var window = new Window(1280, 720, "Wraithspire Engine");
+            window.Run();
+        }
+    }
+
+}
